38a5f82feafdc5c99cdb543775306139c6840825
Kris German <kris@hasira.me>
2026-09-25 20:18:37 +0200

    removed old portoflio content

--- components/sections/SectionSeparator.tsx
+++
@@ -1,8 +0,0 @@
-export const SectionSeparator = () => (
-	<div className="relative">
-		<span
-			aria-hidden="true"
-			className="pointer-events-none absolute left-1/2 top-0 h-px w-screen -translate-x-1/2 border-t border-border"
-		/>
-	</div>
-);
